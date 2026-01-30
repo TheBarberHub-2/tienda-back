0f20051e41a011511f639cc9bbe9e7e1182aac1a
PBC5 <160127358+PBC5@users.noreply.github.com>
2026-01-30 08:47:01 +0100

    Cors config, desesperación

--- tienda/src/main/java/com/fpmislata/daw/tienda/config/CorsConfig.java
+++ tienda/src/main/java/com/fpmislata/daw/tienda/config/CorsConfig.java
@@ -12,11 +12,16 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+           public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "http://greatbank.producciondaw.cip.fpmislata.com",
+                                "http://thebarberhub.producciondaw.cip.fpmislata.com",
+                                "http://thebarberhub-client.producciondaw.cip.fpmislata.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
