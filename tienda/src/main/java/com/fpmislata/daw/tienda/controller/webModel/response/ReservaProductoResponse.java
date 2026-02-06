a8886605c9abfac3ddc05e7ca96a6247ab385a6a
Andreu Alfonso <andalfmeg@alu.edu.gva.es>
2026-02-06 09:20:08 +0100

    feat(TheBarberHub-2/Sprints#37): Añadir Reservas Peluqueria Pt.1

--- tienda/src/main/java/com/fpmislata/daw/tienda/controller/webModel/response/ReservaProductoResponse.java
+++ tienda/src/main/java/com/fpmislata/daw/tienda/controller/webModel/response/ReservaProductoResponse.java
@@ -3,9 +3,9 @@ package com.fpmislata.daw.tienda.controller.webModel.response;
 import java.math.BigDecimal;
 
 public record ReservaProductoResponse(
-                Long id,
-                Long productoId,
-                String nombreProducto,
-                BigDecimal precio,
-                Integer duracion) {
+        Long id,
+        Long productoId,
+        String nombreProducto,
+        BigDecimal precio,
+        Integer duracion) {
 }
